6ba57e429cf222eeb4d75dfe04bd27c194e403ab
Daniela <dabelca159@gmail.com>
2026-05-16 16:22:50 -0400

    Creación de Repository y Service

--- ms-usuario/src/main/java/com/centroanimal/ms_usuario/repository/UsuarioRepository.java
+++ ms-usuario/src/main/java/com/centroanimal/ms_usuario/repository/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package com.centroanimal.ms_usuario.repository;
 
-public class UsuarioRepository {
+import com.centroanimal.ms_usuario.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
 }
--- ms-usuario/src/main/java/com/centroanimal/ms_usuario/service/UsuarioService.java
+++ ms-usuario/src/main/java/com/centroanimal/ms_usuario/service/UsuarioService.java
@@ -1,4 +1,35 @@
 package com.centroanimal.ms_usuario.service;
 
+import com.centroanimal.ms_usuario.model.Usuario;
+import com.centroanimal.ms_usuario.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(long id) {
+        return usuarioRepository.findById(id).get();
+    }
+
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public  void delete(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+
 }
