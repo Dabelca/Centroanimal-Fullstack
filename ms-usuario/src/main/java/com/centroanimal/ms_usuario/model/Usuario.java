9adc42a12cc065866f98f71c0bc3de8e1005ef29
Daniela <dabelca159@gmail.com>
2026-05-16 15:42:51 -0400

    Creación de entidad Usuario

--- ms-usuario/src/main/java/com/centroanimal/ms_usuario/model/Usuario.java
+++ ms-usuario/src/main/java/com/centroanimal/ms_usuario/model/Usuario.java
@@ -1,8 +1,45 @@
 package com.centroanimal.ms_usuario.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 
+@Entity
+@Table (name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(unique = true, length = 10, nullable = false)
+    private int rut;
+
+    @Column(nullable = false)
+    private String correo;
+
+    @Column(nullable = false, length = 11)
+    private String telefono;
+
+    @Column(nullable = false)
+    private String direccion;
+
+    @Column(nullable = false)
+    private String clave;
+
+    @Column(nullable = false)
+    private String rol;
+
 
 
 }
