027f118c1256fa45898ee4e736c7b0c379b31174
Daniela <dabelca159@gmail.com>
2026-05-17 16:31:47 -0400

    Controller

--- ms-usuario/src/main/java/com/centroanimal/ms_usuario/controller/UsuarioController.java
+++ ms-usuario/src/main/java/com/centroanimal/ms_usuario/controller/UsuarioController.java
@@ -1,4 +1,31 @@
 package com.centroanimal.ms_usuario.controller;
 
+import com.centroanimal.ms_usuario.model.Usuario;
+import com.centroanimal.ms_usuario.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listar() {
+        List<Usuario> usuarios = usuarioService.findAll();
+        if (usuarios.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(usuarios);
+    }
+
+
 }
