60ec4b8e35ae5241506092cb414c1785910079c2
Daniela <dabelca159@gmail.com>
2026-05-19 21:23:01 -0400

    ms-padrinos

---
+++ ms-padrinos/src/test/java/com/centroanimal/ms_padrinos/MsPadrinosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroanimal.ms_padrinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPadrinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
