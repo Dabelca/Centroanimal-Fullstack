2d84ab863ff53094e15050eb12d961013f980c5a
Daniela <dabelca159@gmail.com>
2026-05-21 23:24:54 -0400

    ms-reportes

--- ms-reportes/src/main/java/com/centroanimal/ms_reportes/controller/ReporteController.java
+++ ms-reportes/src/main/java/com/centroanimal/ms_reportes/controller/ReporteController.java
@@ -1,4 +1,80 @@
 package com.centroanimal.ms_reportes.controller;
 
+import com.centroanimal.ms_reportes.model.Reporte;
+import com.centroanimal.ms_reportes.service.ReporteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/reportes")
 public class ReporteController {
+
+    @Autowired
+    private ReporteService reporteService;
+
+    @GetMapping
+    public ResponseEntity<List<Reporte>> listar(){
+        List<Reporte> reportes = reporteService.findAll();
+        if (reportes.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(reportes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reporte> buscarPorId(@PathVariable Long id){
+        try {
+            Reporte reporte = reporteService.findById(id);
+            return ResponseEntity.ok(reporte);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/fechaReporte/{fechaReporte}")
+    public ResponseEntity<List<Reporte>> buscarPorFechaReporte(@PathVariable LocalDate fechaReporte){
+            List<Reporte> reportes = reporteService.findByFechaReporte(fechaReporte);
+            if(reportes.isEmpty()){
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.ok(reportes);
+    }
+
+
+    @GetMapping("/tipoReporte/{tipoReporte}")
+    public ResponseEntity<List<Reporte>> buscarPorTipoReporte(@PathVariable String tipoReporte){
+        List<Reporte> reportes = reporteService.findByTipoReporte(tipoReporte);
+        if (reportes.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(reportes);
+    }
+
+    @PostMapping
+    public ResponseEntity<Reporte> crear(@RequestBody Reporte reporte){
+        try {
+            Reporte nuevo = reporteService.save(reporte);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevo);
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        try {
+            reporteService.delete(id);
+            return ResponseEntity.noContent().build();
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
+
--- ms-reportes/src/main/java/com/centroanimal/ms_reportes/service/ReporteService.java
+++ ms-reportes/src/main/java/com/centroanimal/ms_reportes/service/ReporteService.java
@@ -1,4 +1,47 @@
 package com.centroanimal.ms_reportes.service;
 
+import com.centroanimal.ms_reportes.model.Reporte;
+import com.centroanimal.ms_reportes.repository.ReporteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@Transactional
 public class ReporteService {
+
+    @Autowired
+    private ReporteRepository reporteRepository;
+
+    public List<Reporte> findAll(){
+        return reporteRepository.findAll();
+    }
+
+    public Reporte findById(Long id){
+        return reporteRepository.findById(id).get();
+    }
+
+    public Reporte save(Reporte reporte) {
+        if (!List.of("ADOPCIONES", "DONACIONES", "VISITAS", "ANIMALES", "PADRINOS").contains(reporte.getTipoReporte())){
+            throw new RuntimeException("Tipo de reporte no valido.");
+        }
+        reporte.setFechaReporte(LocalDate.now());
+        return reporteRepository.save(reporte);
+    }
+
+    public void delete(Long id) {
+        reporteRepository.deleteById(id);
+    }
+
+    public List<Reporte> findByTipoReporte(String tipoReporte) {
+        return reporteRepository.findByTipoReporte(tipoReporte);
+    }
+
+    public List<Reporte> findByFechaReporte(LocalDate fechaReporte) {
+        return reporteRepository.findByFechaReporte(fechaReporte);
+    }
+
 }
