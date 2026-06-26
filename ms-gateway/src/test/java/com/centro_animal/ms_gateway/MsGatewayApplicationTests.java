81cb098cd96396cd9f047d3041f3e6fd12300958
Daniela <dabelca159@gmail.com>
2026-06-25 21:58:51 -0400

    Faltantes

---
+++ ms-gateway/src/test/java/com/centro_animal/ms_gateway/MsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centro_animal.ms_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
